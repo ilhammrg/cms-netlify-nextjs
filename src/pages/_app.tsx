dd01e19c7722413783a90455e92e04b8f4d389fd
Ilham Gunawan <ilhammrg@gmail.com>
2021-09-11 05:23:18 +0700

    update _app.tsx: init progressbar

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,7 +3,20 @@ import { AppProps } from "next/app";
 // NOTE: Do not move the styles dir to the src.
 // They are used by the Netlify CMS preview feature.
 import "../../public/styles/global.css";
+import React from "react";
+import NextNprogress from "nextjs-progressbar";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <React.Fragment>
+      <NextNprogress
+        color="#2c3e50"
+        startPosition={0.3}
+        stopDelayMs={200}
+        height={3}
+        showOnShallow={true}
+      />
+      <Component {...pageProps} />
+    </React.Fragment>
+  );
 }
