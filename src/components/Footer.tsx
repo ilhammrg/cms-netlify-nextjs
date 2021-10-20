bb7383108bf3d21a0f3d6371881ff3d169036854
Ilham Gunawan <ilhammrg@gmail.com>
2021-10-20 21:04:08 +0700

    update footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,12 +1,7 @@
 export default function Footer() {
   return (
     <footer className="px-4 my-5">
-      <p className="font-normal text-gray-400">
-        {"Powered by  "}
-        <a className="font-semibold text-gray-500 hover:text-black" href="https://nextjs.org" target="_blank" referrerPolicy="no-referrer">Next.js</a> 
-        {" & "}
-        <a className="font-semibold text-gray-500 hover:text-black" href="https://netlifycms.org" target="_blank" referrerPolicy="no-referrer">Netlify CMS</a>
-      </p>
+      <span className='font-normal text-gray-500'>All rights reserved © Ilham Gunawan 2021</span>
       <style jsx>{`
         @media (min-width: 769px) {
           footer {
