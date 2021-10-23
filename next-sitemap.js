6516bda333efcedb471f537b522d28048df45d60
Ilham Gunawan <ilhammrg@gmail.com>
2021-10-23 12:43:16 +0700

    update sitemap config

--- next-sitemap.js
+++ next-sitemap.js
@@ -1,5 +1,4 @@
 module.exports = {
-  siteUrl: process.env.SITE_URL || 'https://ilhamgunawan.my.id',
+  siteUrl: 'https://ilhamgunawan.my.id',
   generateRobotsTxt: false,
-  exclude: ['/sitemap.xml'],
 }
